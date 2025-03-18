6516fe26f4c0d672f4c91d70048317cc20ec98e0
Filza548 <filzajameel458@gmail.com>
2025-03-19 01:07:03 +0500

    completed

---
+++ wheather.js
@@ -0,0 +1,35 @@
+let wheatherInp = +prompt("Check the weather in your city! ")
+
+
+if (wheatherInp >= 46){
+    document.write("<img src='images/29ff54c4-34f7-4c1a-8a4a-a741c7279abe.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+}
+else if (wheatherInp >= 41){
+    document.write("<img src='images/fbd79f9a-17f8-4755-ab48-4b9239c4fc16.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+}
+else if (wheatherInp >= 36){
+    document.write("<img src='images/91c6259e-565c-426c-aecc-df7ebe59929a.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+}
+else if (wheatherInp >= 31){
+    document.write("<img src='images/1fca68a3-d055-4406-8cec-012bfb778a7e.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+}
+else if (wheatherInp >= 26){
+    document.write("<img src='images/ed8d7f0f-a8a2-469f-8edd-57457a31303a.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+}
+else if (wheatherInp >= 21){
+    document.write("<img src='images/5605da9b-ba3f-4d3a-a6eb-6e4dadde30c1.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+}
+ else if (wheatherInp >= 16){
+    document.write("<img src='images/c91c9839-a9c7-49dd-be22-71fc3a991c15.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+}
+        else if (wheatherInp >= 10) {
+            document.write("<img src='images/11degree.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+        } else if (wheatherInp >= 5) {
+            document.write("<img src='images/fff2ed1a-58b6-4d15-93ed-e251916af82d.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+        } else if (wheatherInp >= 1){
+            document.write("<img src='images/540ed015-1f50-4029-9a5b-87088fd15eba.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+        }else{
+            document.write("<img src='images/ac0e098b-75ba-4af6-8948-161b64694ff6.webp' alt='Winter' width='500px' style='display:block; margin:0 auto;'>");
+        }
+
+        
